de6680ee65126c039166c8c981e5339a368e94de
Alexandre Pereira <alex@gooood.biz>
2024-04-17 18:11:25 +0900

    adding support for unspecified command : default to query

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,52 @@
 
 import { Command } from 'commander';
 import { configCommand } from './commands/config';
-import { queryCommand } from './commands/query'; 
+import { queryCommand } from './commands/query';
 import { initCommand } from './commands/init';
 import { agentsCommand } from './commands/agents';
 
 const program = new Command();
 
-program.name('2501').description('An AI to rule your systems');
+program
+  .name('2501')
+  .description('An AI to rule your systems')
+  .version('0.0.1')
+  .on('command:*', async (...args) => {
+    const query = args[0] && args[0].join(' ');
+    // @TODO : implement options support.
+    await queryCommand(query, {});
+  });
 
 // Config command
 program
-  .command('config')  
+  .command('config')
   .description('Fetch configuration from API')
   .action(configCommand);
 
-// Query command  
+// Query command
 program
-  .command('query <query>')
+  .command('query')
+  .argument('<query>', 'Query to execute')
   .description('Execute a query using the specified agent')
   .option('--workspace <path>', 'Specify a different workspace path')
   .option('--agentId <id>', 'Specify the agent ID')
   .action(queryCommand);
 
 // Init command
-program  
+program
   .command('init')
   .description('Initializes a new agent')
   .option('--name <name>', 'Specify the name of the agent')
-  .option('--workspace <path>', 'Specify a different workspace path') 
+  .option('--workspace <path>', 'Specify a different workspace path')
   .option('--config <config_id>', 'Specify the configuration ID')
   .action(initCommand);
 
 // Agents command
 program
   .command('agents')
-  .description('List agents in the current workspace or all agents on the machine')
+  .description(
+    'List agents in the current workspace or all agents on the machine'
+  )
   .option('--workspace <path>', 'Specify a different workspace path')
   .option('--all', 'List all agents on the machine')
   .option('--flush', 'Flush all agents from the configuration')
