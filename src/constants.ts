fdb97ac26fff44c8d14139919a0616124eec2a40
Alexandre Pereira <alex@gooood.biz>
2024-06-05 15:29:03 +0900

    dynamic const depending on env

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // src/constants/api.js
 
 // API Configuration 
-export const API_HOST = 'http://localhost:1337';
+export const API_HOST = process.env.NODE_ENV === 'dev' ? 'http://localhost:1337' : 'https://engine.2501.ai';
 export const API_VERSION = '/api/v1';
