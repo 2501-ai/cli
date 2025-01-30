ec25407de6f15ebbfd2aa95892225dadc7d4c3cf
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-01-30 17:23:13 +0100

    feat: added tests coverage

--- jest.config.js
+++ jest.config.js
@@ -11,11 +11,15 @@ module.exports = {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
+  // Enable coverage collection
+  collectCoverage: true,
+
+  // Configure coverage reporting
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    // "**/__tests__/**/*.[jt]s?(x)",
-    '**/?(*.)+(spec|test).[tj]s?(x)',
-  ],
+  testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['node_modules', 'dist'],
