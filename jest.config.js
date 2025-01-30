a53214012e981a1ef39206e91b531b2dfbb1b418
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-01-30 17:33:55 +0100

    feat: remove coverage

--- jest.config.js
+++ jest.config.js
@@ -8,16 +8,6 @@ module.exports = {
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
-
-  // Enable coverage collection
-  collectCoverage: true,
-
-  // Configure coverage reporting
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-
   // The glob patterns Jest uses to detect test files
   testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)'],
 
