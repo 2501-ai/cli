08f37c38dc43bdb8cd92a68691477655b20a3ac6
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-06-06 10:32:23 -0500

    feat: updated windows installer

--- installers/windows-installer.ps1
+++ installers/windows-installer.ps1
@@ -74,7 +74,7 @@ try {
         
         # Install 2501-ai/cli
         Write-Host "Installing @2501-ai/cli..." -ForegroundColor Yellow
-        & npm install -g "@2501-ai/cli@0.2.22-alpha-windows-2"
+        & npm install -g "@2501-ai/cli@0.2.22-alpha-windows-3"
         
         if ($LASTEXITCODE -eq 0) {
             $cliVersion = & tz --version 2>$null
