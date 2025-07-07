c56778e5970bef7540e338e55f803784a04a5ec6
Bonnard Valentin <val@2501.ai>
2025-07-07 10:39:18 +0200

    fix(pruned_message) : disable protection about big files

--- src/managers/agentManager.ts
+++ src/managers/agentManager.ts
@@ -151,18 +151,7 @@ export class AgentManager {
         output += `\n\n NOTE: your original content for ${args.path} was corrected with the new version below before running the function: \n\n${args.content}`;
       }
 
-      if (output.length > 20000) {
-        output = `
-        ERROR: The output is too large to display to prevent performance issues.
-        Use an alternative method to retrieve the relevant information for the user (for example grep, an another command or sample the content first).
-        `;
-
-        return {
-          tool_call_id: action.id,
-          output,
-          success: false,
-        };
-      }
+      /*M */
 
       return {
         tool_call_id: action.id,
