9d45af428cc23b4a4d6fa83f1a3207bb05f7960e
Alex Zhuk <alex.a.zhuk@gmail.com>
2025-07-18 13:24:32 +0200

    TFZO_UPDATED: true back

--- src/utils/cliUpdate.ts
+++ src/utils/cliUpdate.ts
@@ -70,6 +70,7 @@ export async function handleAutoUpdate(): Promise<boolean> {
       stdio: 'inherit',
       env: {
         ...process.env,
+        TFZO_UPDATED: 'true',
       },
     });
   } catch (error) {
