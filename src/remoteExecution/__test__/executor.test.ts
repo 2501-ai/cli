a221fdfc67f0fed86b146fcbc2adf6cd53dc140e
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-07-22 10:17:20 +0200

    feat: skipped manual test

--- src/remoteExecution/__test__/executor.test.ts
+++ src/remoteExecution/__test__/executor.test.ts
@@ -6,18 +6,22 @@ if (!JEST_WINRM_PASS || !JEST_WINRM_USER || !JEST_WINRM_HOST) {
   throw new Error('Missing environment variables');
 }
 // Ex usage : npx dotenv -e .env.test -- npx jest executor.test.ts
-describe('RemoteExecutor - WinRM', () => {
+// Skip it not to make it run in the CI.
+describe.skip('RemoteExecutor - WinRM', () => {
   beforeAll(async () => {
-    RemoteExecutor.instance.init({
-      enabled: true,
-      target: JEST_WINRM_HOST,
-      port: 5985,
-      type: 'winrm',
-      platform: 'windows',
-      user: JEST_WINRM_USER,
-      remote_workspace: `C:\\Users\\${JEST_WINRM_USER}`,
-      password: JEST_WINRM_PASS,
-    });
+    RemoteExecutor.instance.init(
+      {
+        enabled: true,
+        target: JEST_WINRM_HOST,
+        port: 5985,
+        type: 'winrm',
+        platform: 'windows',
+        user: JEST_WINRM_USER,
+        remote_workspace: `C:\\Users\\${JEST_WINRM_USER}`,
+        password: JEST_WINRM_PASS,
+      },
+      '/tmp/test'
+    );
     await RemoteExecutor.instance.validateConnection();
   });
 
