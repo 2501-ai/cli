bc221ee094169f9fbdbde85f658e53d7abf984fc
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-12-10 19:17:46 +0100

    feat: added missing eslint file

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const globals = require('globals');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', '.debug-workspace/**', '*.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'no-case-declarations': 'off',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-control-regex': 'off',
+    },
+  }
+);
